97d15b607e743a9fd57dab75db5691cb916e885b
Atheer Alharthi <atheerabdullahalharthi@gmail.com>
2026-04-15 02:44:58 +0300

    Complete the blog project requirements and finalize the basic features

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render, redirect, get_object_or_404 # أضفنا get_object_or_404 هنا
+from django.shortcuts import render
 from .models import Post
 
-# دالة إضافة المنشور
+# (دالة إضافة المنشور )
 def add_post_view(request):
     if request.method == "POST":
+       
         new_post = Post(
             title=request.POST["title"],
-            content=request.POST["content"],
-            is_published=request.POST.get("is_published", True)
+            content=request.POST["content"]
         )
         new_post.save()
-        return redirect("main:home_view")
+        return render(request, "blog/add_post.html") 
     
     return render(request, "blog/add_post.html")
 
+
+def home_view(request):
+    posts = Post.objects.all()
+    return render(request, "main/home.html", {"posts": posts})
