7f3753e1b31534b20a405a466afc2aa5919651c7
Atheer Alharthi <atheerabdullahalharthi@gmail.com>
2026-04-16 01:25:21 +0300

    feat: implement post update functionality with premium editor UI

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = "blog"
 urlpatterns = [
     path('add/', views.add_post_view, name='add_post_view'),
     path("detail/<post_id>/", views.post_detail_view, name="post_detail_view"),
+    path("post/update/<post_id>/", views.post_update_view, name="post_update_view"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpRequest
 from .models import Post
 
-# (دالة إضافة المنشور )
-def add_post_view(request):
+# دالة إضافة المنشور
+def add_post_view(request: HttpRequest):
     if request.method == "POST":
-       
         new_post = Post(
             title=request.POST["title"],
-            content=request.POST["content"]
+            content=request.POST["content"],
+            
+            poster=request.FILES.get("poster") 
         )
         new_post.save()
-        return render(request, "blog/add_post.html") 
+        return redirect("main:home_view") # يفضل الرديركت بعد الحفظ
     
     return render(request, "blog/add_post.html")
 
-
-def home_view(request):
-    posts = Post.objects.all()
+# الصفحة الرئيسية
+def home_view(request: HttpRequest):
+    posts = Post.objects.all().order_by("-published_at")
     return render(request, "main/home.html", {"posts": posts})
 
-def post_detail_view(request:HttpRequest, post_id:int):
-    post = Post.objects.get(pk=post_id)
+# صفحة التفاصيل البحث بالـ ID
+def post_detail_view(request: HttpRequest, post_id: int):
+    post = get_object_or_404(Post, pk=post_id)
     return render(request, "blog/post_detail.html", {"post": post})
+
+# صفحة التعديل
+def post_update_view(request: HttpRequest, post_id: int):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == "POST":
+        post.title = request.POST.get("title", post.title)
+        post.content = request.POST.get("content", post.content)
+        
+        
+        if "poster" in request.FILES:
+            post.poster = request.FILES["poster"]
+        
+        if "video" in request.FILES:
+            post.video = request.FILES["video"]
+
+        if "media" in request.FILES:
+            post.media = request.FILES["media"]
+
+        post.save()
+        
+        return redirect("blog:post_detail_view", post_id=post.id)
+
+    return render(request, "blog/post_update.html", {"post": post})
